6945bc035477c73fa0a38c3f7fed3e614724cbef
euCabele <euCabele@gmail.com>
2026-04-14 21:06:06 -0300

    req1: define cadastroVIEW como a tela principal

---
+++ src/LeiloesTDSat.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author caleb
+ */
+public class LeiloesTDSat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         /* Inicia a aplicação pela tela de cadastroVIEW*/
+        java.awt.EventQueue.invokeLater(() -> {
+            new cadastroVIEW().setVisible(true);
+        });
+    }
+    
+}
